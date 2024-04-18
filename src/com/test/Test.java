50082f8e29032493e9b2efb491c94c72ee7943cd
anischerni <anischerni23@gamil.com>
2024-04-18 15:57:26 +0200

    first test avec java

---
+++ src/com/test/Test.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Test {
+	
+	
+	public static void main(String[]args) {
+		
+	int leght = 180;
+	int width =120;
+	
+ float perim=(float)width/leght;
+ System.out.println("la valeur est = " + perim);
+		
+	}
+
+}
